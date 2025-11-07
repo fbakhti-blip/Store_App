a9a44153ee3ced0c0895f391f041048165c696d4
fbakhti-blip <fbakhti@gmail.com>
2025-11-07 15:11:29 +0330

    Modified

--- repository/warehouse_repository.py
+++ repository/warehouse_repository.py
@@ -58,14 +58,14 @@ class WarehouseRepository:
 
     def find_by_quantity_less_than(self, quantity):
         self.connect()
-        self.cursor.execute("select * from warehouses where quantity < ?", [quantity])
+        self.cursor.execute("select * from warehouses where quantity <= ?", [quantity])
         warehouse_list = [Warehouse(*warehouse) for warehouse in self.cursor.fetchall()]
         self.disconnect()
         return warehouse_list
 
     def find_by_quantity_more_than(self, quantity):
         self.connect()
-        self.cursor.execute("select * from warehouses where quantity > ?", [quantity])
+        self.cursor.execute("select * from warehouses where quantity >= ?", [quantity])
         warehouse_list = [Warehouse(*warehouse) for warehouse in self.cursor.fetchall()]
         self.disconnect()
         return warehouse_list
